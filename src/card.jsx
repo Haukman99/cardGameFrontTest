6c6ddaf6822bf768c9c45b56eac843375303c99d
Haukman99 <h.nierling@osnanet.de>
2022-05-12 11:19:31 +0200

    netfly3

--- src/card.jsx
+++ src/card.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { io } from "socket.io-client";
 
-const socket = io(/*'https://cardtestnode.herokuapp.com/' ||*/ "http://localhost:3001", { autoConnect: false });
+const socket = io('https://cardtestnode.herokuapp.com/'/* || "http://localhost:3001"*/, { autoConnect: false });
 
   export default function Card() {
     const [input, setInput] = useState('');
